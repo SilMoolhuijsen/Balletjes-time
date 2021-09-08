22d7d925728dce964ec7b6d0a7e3f829d76abc99
SilMoolhuijsen <74902979+SilMoolhuijsen@users.noreply.github.com>
2021-09-08 12:05:30 +0000

    1

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-var x, y;
+var x, y, v, s;
 
 function setup() {
 	createCanvas(500, 400);
 
-  x = 0;
-  y = 200;
+  x = 30;
+  y = 30;
+  v = 4;
+  s = 4;
 }
 
 function draw() {
 	background(225);
   
   ellipse(x,y,50,50);
-  x = x + 5;
+  x = x + v;
+  y = y + s;
 
-  if(x > 500){
-    x = 0;
+  if(x <= 30 || x >= 470){
+    v = v * -1;
+  }
+  if(y <= 30 || y >= 370){
+    s = s * -1;
   }
 }
